ea5219886e3cf5e10d40e9c0c77daf3716d4e66f
thrombe <yash.n.thombare620@gmail.com>
2021-03-29 04:48:13 +0530

    did a bit of cleanup in try2

--- sudokuTry2.py
+++ sudokuTry2.py
@@ -1,36 +1,5 @@
 
-'''
-TODO
-    sudoku generator - very hard. basically try to solve after removing stuff for single sol puzzles
-    more human like method of solving
-        store a list of possible values of each square and put the value in if only 1 available. then remove this value from the corresponding lists of squares
-        integrate both methods for faster search
-    if not previous method, randomise what/ numbers and squares to try
-
-text
-    create square objects
-    square.hori, square.vert, square.box, square.values, 
-    if a value is confirmed, remove that value from all objects from square.hori,vert,box
-    how to try values that we cant confirm? (no square.values has just 1 value left):
-        copy.copy() objects in a recursive function
-    while removing values from square.values, check if only 1 value remaining. and maybe also 2 and 3 for priority
-    maintain list of empty squares for loop (was already doing this)
-
-    how this helps?:
-        dont have to loop through all squares just to check the if it lies in same box etc
-        square.values would all for checking only useful values and not check if its valid
-'''
 import copy
-
-def legal(board, square, val):
-    for coor, sq in board.items():
-        if sq == square: break
-    for coor2, sq in board.items():
-        if coor[0] in coor2 or coor[1] in coor2 or coor[2] in coor2:
-            if type(sq.values) == type(0):
-                if val == sq.values: return False
-    return True
-
 
 class Square:
     def __init__(self, neighbours, values):
@@ -38,18 +7,11 @@ class Square:
         self.values = values
     
     def setValue(self, value, blanks, board): # sets the value and discards it from neghbour's values
-        #print(self.values)#
-        #if not legal(board, self, value): return False
         self.values = value
-        blanks.remove(self)
+        blanks.discard(self)
         for neighbour in self.neighbours:
-            #print(neighbour in blanks)###
-            #print(value, neighbour.values)###
             neighbour.values.discard(value)
-            #print(neighbour.values)###
-            if neighbour.values == set():
-                #print('fal1')####
-                return False # if no value is suitable for a cell, something went wrong
+            if neighbour.values == set(): return False # if no value is suitable for a cell, something went wrong
             neighbour.neighbours.discard(self)
         return True # if nothing went wrong
 
@@ -86,77 +48,45 @@ def printBoard(board, playing = 'no'): # prints the board
 
 def EZsquares(blanks, board): # eliminates easy squares with only 1 possible value
     blanksC = blanks.copy() # so that it dosent change while loop
+    num, least = 10, None
     for square in blanksC:
         length = len(square.values)
         if length == 1:
             valueResult = square.setValue(list(square.values)[0], blanks, board)
-            if valueResult == False:
-                #print('fal2')###
-                return False
-    return True
+            if valueResult == False: return False
+        elif length < num: least, num = square, length
+    return least
 
 def EZloop(board, blanks): # loops eliminating easy squares till no ez squares left
     length = 0
     while length != len(blanks):
         length = len(blanks)
         valueResult = EZsquares(blanks, board)
-        if valueResult == False:
-            #print('fal3')###
-            return False
-    if len(blanks) == 0:
-        #print('bor3')
-        return board
-
+        if valueResult == False: return False
+    if len(blanks) == 0: return board
+    return valueResult
+    
 def solve(board, blanks, mainSolve = 0):
-    #######
-    #print(len(blanks))###
-    if len(blanks) == 0:
-        printBoard(board)###
-        #return board
-    ########
-
     valueResult = EZloop(board, blanks)
-    if valueResult == False: 
-        #print('fal4')###
-        return False
-    elif type(valueResult) == type({'key': 'val'}):
-        #print('bor4')###
-        return board
+    if valueResult == False: return False
+    elif type(valueResult) == type({'key': 'val'}): return board
+    least = valueResult
 
-    #return board##
     # logic for when every element in blanks has multiple possible values
-    num = 10
-    for square in blanks: # shift this inside EZsquares
-        if len(square.values) < num:
-            least = square
-            num = len(square.values)
-    #print(least.values)##
-
     leastVal = least.values.copy()
     for val in leastVal:
-        neiVal = [nei for nei in least.neighbours if val in nei.values]
+        neiVal = [nei for nei in least.neighbours if val in nei.values] # record what neighbours used to contain val before guess
         valueResult = least.setValue(val, blanks, board) # take a guess at what the value might be
         if valueResult == False: continue
-        boardNew = copy.deepcopy(board)
-        blanksNew = set()
-        num = 10
+        boardNew, blanksNew = copy.deepcopy(board), set()
         for square in boardNew.values(): #create a copy of board and blanks
             if type(square.values) == type(set()): blanksNew.add(square)
         valueResult = solve(boardNew, blanksNew)
-        #if valueResult == False: return False
-        if type(valueResult) == type({'key' : 'val'}):
-            #print('bor5', len(valueResult))###
-            #printBoard(valueResult)###
-            return valueResult
-        #for neighbour in least.neighbours: # undo the guess we took
-        #    neighbour.values.add(val)
+        if type(valueResult) == type({'key' : 'val'}): return valueResult
         for nei in neiVal: nei.values.add(val)
-        blanks.add(least)
         least.values = leastVal
-    #print('fal6')###
     return False
 
-    
 
 if __name__ == '__main__':
     
@@ -180,13 +110,12 @@ if __name__ == '__main__':
     import time
     start = time.time()
 
-    def sob():
+    def startSolve():
         board, blanks = genBoard(a)
         board = solve(board, blanks, 1)
+        if board != False: printBoard(board)
+        else: print('board is janked')
         return board
     
-    board = sob()
-    printBoard(board)
-    #for s in board.values(): print(s.values)####
-    #printBoard(board)
+    startSolve()
     print(time.time() - start)
